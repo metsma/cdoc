765879cc88f7722fa67b6d5199632bd1afc131ef
Raul Metsma <raul@metsma.ee>
2018-01-09 14:59:24 +0200

    Handle Windows locale files
    IB-4959

--- main.cpp
+++ main.cpp
@@ -8,9 +8,56 @@
 #include <iostream>
 #include <fstream>
 
+#ifdef _WIN32
+#include <Windows.h>
+
+static std::wstring toWide(UINT codePage, const std::string &in)
+{
+	int len = MultiByteToWideChar(codePage, 0, in.data(), int(in.size()), nullptr, 0);
+	std::wstring result(size_t(len), 0);
+	len = MultiByteToWideChar(codePage, 0, in.data(), int(in.size()), &result[0], len);
+	return result;
+}
+
+static std::string toMultiByte(UINT codePage, const std::wstring &in)
+{
+	int len = WideCharToMultiByte(codePage, 0, in.data(), int(in.size()), nullptr, 0, nullptr, nullptr);
+	std::string result(size_t(len), 0);
+	len = WideCharToMultiByte(codePage, 0, in.data(), int(in.size()), &result[0], len, nullptr, nullptr);
+	return result;
+}
+#endif
+
+static std::string toUTF8(const std::string &in)
+{
+	if(in.empty())
+		return in;
+#ifdef _WIN32
+	return toMultiByte(CP_UTF8, toWide(CP_ACP, in));
+#else
+	return in;
+#endif
+}
+
+static std::string fromUTF8(const std::string &in)
+{
+	if(in.empty())
+		return in;
+#ifdef _WIN32
+	return toMultiByte(CP_ACP, toWide(CP_UTF8, in));
+#else
+	return in;
+#endif
+}
+
 static std::vector<unsigned char> readFile(const std::string &path)
 {
+#ifdef _WIN32
+	std::wstring wide = toWide(CP_UTF8, path);
+	std::ifstream f(wide.c_str(), std::ifstream::binary);
+#else
 	std::ifstream f(path, std::ifstream::binary);
+#endif
 	f.seekg(0, std::ifstream::end);
 	std::vector<unsigned char> data(size_t(f.tellg()), 0);
 	f.clear();
@@ -19,48 +66,51 @@ static std::vector<unsigned char> readFile(const std::string &path)
 	return data;
 }
 
+static void writeFile(const std::string &path, const std::vector<unsigned char> &data)
+{
+#ifdef _WIN32
+	std::wstring wide = toWide(CP_UTF8, path);
+	std::ofstream f(wide.c_str());
+#else
+	std::ofstream f(path.c_str());
+#endif
+	f.write((const char*)data.data(), std::streamsize(data.size()));
+}
+
 int main(int argc, char *argv[])
 {
 	if(argc >= 5 && strcmp(argv[1], "encrypt") == 0)
 	{
-		CDOCWriter w(argv[argc-1], "http://www.w3.org/2009/xmlenc11#aes256-gcm");
+		CDOCWriter w(toUTF8(argv[argc-1]), "http://www.w3.org/2009/xmlenc11#aes256-gcm");
 		for(int i = 3; i < argc - 1; ++i)
 		{
-			std::string inFile = argv[i];
+			std::string inFile = toUTF8(argv[i]);
 			size_t pos = inFile.find_last_of("/\\");
 			w.addFile(pos == std::string::npos ? inFile : inFile.substr(pos + 1), "application/octet-stream", readFile(inFile));
 		}
-		w.addRecipient(readFile(argv[2]));
+		w.addRecipient(readFile(toUTF8(argv[2])));
 		w.encrypt();
 	}
 	else if(argc == 7 && strcmp(argv[1], "decrypt") == 0)
 	{
 		std::unique_ptr<Token> token;
 		if (strcmp(argv[2], "pkcs11") == 0)
-			token.reset(new PKCS11Token(argv[3], argv[4]));
+			token.reset(new PKCS11Token(toUTF8(argv[3]), argv[4]));
 		else if (strcmp(argv[2], "pkcs12") == 0)
-			token.reset(new PKCS12Token(argv[3], argv[4]));
+			token.reset(new PKCS12Token(toUTF8(argv[3]), argv[4]));
 #ifdef _WIN32
 		else if (strcmp(argv[2], "win") == 0)
 			token.reset(new WinToken(strcmp(argv[3], "ui") == 0, argv[4]));
 #endif
-		CDOCReader r(argv[5]);
+		CDOCReader r(toUTF8(argv[5]));
 		std::vector<unsigned char> data = r.decryptData(token.get());
 		if(r.mimeType() == "http://www.sk.ee/DigiDoc/v1.3.0/digidoc.xsd")
 		{
 			for(const DDOCReader::File &file: DDOCReader::files(data))
-			{
-				std::string path = std::string(argv[6]) + "/" + file.name;
-				std::ofstream f(path.c_str());
-				f.write((const char*)file.data.data(), std::streamsize(file.data.size()));
-			}
+				writeFile(toUTF8(argv[6]) + "/" + file.name, file.data);
 		}
 		else
-		{
-			std::string path = std::string(argv[6]) + "/" + r.fileName();
-			std::ofstream f(path.c_str());
-			f.write((const char*)data.data(), std::streamsize(data.size()));
-		}
+			writeFile(toUTF8(argv[6]) + "/" + r.fileName(), data);
 	}
 	else
 	{
